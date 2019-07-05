636ee7b85c0617a3fba34a989b97749b57b51af6
Anton Dort-Golts <dortgolts@gmail.com>
2019-07-05 23:37:30 +0300

    [Impl] multiple vertex descendants in CLI args

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -27,15 +27,17 @@ fn main() {
             let formatted: Vec<String> = order.into_iter().map(|s| s.to_owned()).collect();
             println!("Topological order found:\n{}", formatted.join(" -> "))
         }
-        Err(TSortErr::NoOrder) => eprintln!("no topological sorting order could be found for given graph"),
+        Err(TSortErr::NoOrder) => {
+            eprintln!("no topological sorting order could be found for given graph")
+        }
         Err(TSortErr::Cycle) => eprintln!("no sorting possible: cycles detected in graph"),
     }
 }
 
 fn print_usage() {
     println!("Utility tsrt makes topological sort of relation graph.");
-    println!("Provide it with a set of space-delimited relations, represented");
-    println!("as comma-separated pairs 'X,Y', where vertex X precedes Y in the DAG.\n");
+    println!("Provide it with a set of space-delimited relations, represented as a");
+    println!("comma-separated pairs 'X,Y[,Z...]', where vertex X precedes Y in the DAG.\n");
     println!("Output will be a topological ordering of all the vertices.");
 }
 
@@ -48,14 +50,17 @@ fn parse_relations(rels: Vec<String>) -> Result<Vec<Relation<String>>, ParseErr>
     let mut result = Vec::with_capacity(rels.len());
     for rel in rels.into_iter() {
         let relation: Vec<_> = rel.split(",").collect();
-        if relation.len() != 2 {
+        if relation.len() < 2 {
             return Err(ParseErr::BadRelation(relation.as_slice().join(",")));
         }
 
-        result.push(Relation {
-            from: relation[0].to_string(),
-            to: relation[1].to_string(),
-        })
+        let from = relation[0];
+        relation.iter().skip(1).for_each(|to| {
+            result.push(Relation {
+                from: from.to_string(),
+                to: to.to_string(),
+            })
+        });
     }
 
     Ok(result)
